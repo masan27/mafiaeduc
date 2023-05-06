a2aaa8dd8f16a5c04b617a0cab00422e4b22055c
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-05-06 16:40:38 +0700

    add interface service provider to use interface pattern

---
+++ app/Providers/InterfaceServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InterfaceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Register the Service Interfaces
+        // -------------------------------
+
+        $this->app->bind(
+            \App\Services\WelcomeServiceInterface::class,
+            \App\Services\WelcomeService::class
+        );
+
+        // Register the Repository Interfaces
+        // -------------------------------
+    }
+}

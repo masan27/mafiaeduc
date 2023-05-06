709a58ae80d054f2b7615df48d96d55ea306cb34
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-05-06 16:41:05 +0700

    add repo traits

--- app/Traits/RepoTrait.php
+++ app/Traits/RepoTrait.php
@@ -5,40 +5,40 @@ namespace App\Traits;
 trait RepoTrait
 {
 
-  abstract static function getDbTable();
-
-  public static function getAll(array $columns = array('*'))
-  {
-    return self::getDbTable()->select(...$columns)->get();
-  }
-
-  public static function findById($primaryId, array $columns = array('*'), string $primaryFieldName = 'id')
-  {
-    return self::getDbTable()->select(...$columns)->where($primaryFieldName, $primaryId)->first();
-  }
-
-  public static function arrayWhere(array $conditions = array(), array $columns = array('*'))
-  {
-    $query = self::getDbTable()->select(...$columns);
-
-    if (gettype($conditions[0]) === 'array') {
-      foreach ($conditions as $condition) {
-        $query->where(...$condition);
-      }
-    } else {
-      $query->where(...$conditions);
+    public static function getAll(array $columns = array('*'))
+    {
+        return self::getDbTable()->select(...$columns)->get();
     }
 
-    return $query;
-  }
+    abstract static function getDbTable();
 
-  public static function update($whereValue,string $whereField = "id", array $data = array())
-  {
-    return self::getDbTable()->where($whereField, $whereValue)->update($data);
-  }
+    public static function findById($primaryId, array $columns = array('*'), string $primaryFieldName = 'id')
+    {
+        return self::getDbTable()->select(...$columns)->where($primaryFieldName, $primaryId)->first();
+    }
 
-  public static function insert(array $data = array())
-  {
-    return self::getDbTable()->insert($data);
-  }
+    public static function arrayWhere(array $conditions = array(), array $columns = array('*'))
+    {
+        $query = self::getDbTable()->select(...$columns);
+
+        if (gettype($conditions[0]) === 'array') {
+            foreach ($conditions as $condition) {
+                $query->where(...$condition);
+            }
+        } else {
+            $query->where(...$conditions);
+        }
+
+        return $query;
+    }
+
+    public static function update($whereValue, string $whereField = "id", array $data = array())
+    {
+        return self::getDbTable()->where($whereField, $whereValue)->update($data);
+    }
+
+    public static function insert(array $data = array())
+    {
+        return self::getDbTable()->insert($data);
+    }
 }

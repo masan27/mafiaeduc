2a796ef2ba4a2bd77e7fe73ffbc0d2d25075b27c
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-05-06 16:47:38 +0700

    update user migration tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Entities\RoleEntities;
+use App\Entities\UserEntities;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(UserEntities::USER_ACTIVE);
+            $table->tinyInteger('role')->default(RoleEntities::GUEST_ROLE);
             $table->rememberToken();
             $table->timestamps();
         });

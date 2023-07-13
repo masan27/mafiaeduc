6ec30bc09e6509f21657d2bea3790c4da8352dcc
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-07-14 01:02:03 +0700

    make payment method seeder

---
+++ database/seeders/PaymentMethods/PaymentMethodSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders\PaymentMethods;
+
+use App\Models\Payments\PaymentMethod;
+use Illuminate\Database\Seeder;
+
+class PaymentMethodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (PaymentMethod::count() > 0) {
+            return;
+        }
+
+        $paymentMethods = [
+            [
+                'name' => 'BCA',
+                'icon' => 'bca.png',
+                'code' => 'bca',
+                'type' => 'bank',
+                'status' => 1,
+                'description' => 'Bank BCA',
+                'fee' => 1500,
+                'account_number' => '1234567890',
+            ]
+        ];
+
+        foreach ($paymentMethods as $paymentMethod) {
+            PaymentMethod::create($paymentMethod);
+        }
+    }
+}

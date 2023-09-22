abd66c80a950aae8949928774024279c15a1f9b5
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-09-22 22:12:03 +0700

    add schedule filter

--- app/Services/Schedules/ScheduleService.php
+++ app/Services/Schedules/ScheduleService.php
@@ -5,18 +5,23 @@ namespace App\Services\Schedules;
 use App\Helpers\FileHelper;
 use App\Helpers\ResponseHelper;
 use App\Models\Schedules\Schedule;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ScheduleService implements ScheduleServiceInterface
 {
-    public function getUserSchedules(): array
+    public function getUserSchedules(Request $request): array
     {
         try {
             $userId = Auth::id();
+            $learningMethod = $request->query('learning_method');
 
             $schedules = Schedule::with('mentor:id,full_name,phone,photo', 'grade', 'subject', 'learningMethod', 'privateClass', 'groupClass')
                 ->whereHas('users', function ($query) use ($userId) {
                     $query->where('user_id', $userId);
+                })
+                ->when($learningMethod, function ($query, $learningMethod) {
+                    $query->where('learning_method_id', $learningMethod);
                 })
                 ->orderBy('date', 'desc')
                 ->get();
